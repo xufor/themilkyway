4fcacf0f4522a23bed8f0e2301ad8b78444bcd8e
Xufor <lucky1612000@gmail.com>
2019-02-02 02:37:52 +0530

    Added a story.

--- src/containers/feedBox.js
+++ src/containers/feedBox.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { sampleStory } from "../strings";
 import '../css/feedBox.css';
 
 class FeedBox extends Component {
     render() {
-        return <div>{}</div>;
+        return (
+            <div id={'boxWrapper'}>
+                <p>{ sampleStory }</p>
+            </div>
+        );
     }
 }
 
