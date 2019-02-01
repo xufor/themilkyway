9d0547e454dd7c1aa65054bd3763d9aac9236c93
Xufor <lucky1612000@gmail.com>
2019-02-01 23:52:08 +0530

    Commented a few lines.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import WelcomePage from './components/welcomePage.js';
-import LoginPage from './containers/loginPage.js';
+// import WelcomePage from './components/welcomePage.js';
+// import LoginPage from './containers/loginPage.js';
 import HomePage from './containers/homePage.js';
-import RegisterPage from './containers/registerPage.js';
+// import RegisterPage from './containers/registerPage.js';
 import {BrowserRouter, Route} from "react-router-dom";
 import './App.css';
 
@@ -20,7 +20,7 @@ class App extends Component {
           </BrowserRouter>
       );
   };
-};
+}
 
 
 export default App;
