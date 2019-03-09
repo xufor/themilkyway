97992871364d15a623de4e0f78a51dc4662d0783
Abu Nasar <abunasar388@gmail.com>
2019-03-09 23:30:41 +0530

    i made changes

--- src/containers/profilePage/profilePage.js
+++ src/containers/profilePage/profilePage.js
@@ -3,18 +3,13 @@ import PageFooter from '../../components/pageFooter/pageFooter';
 import TopMostBar from '../topMostBar/topMostBar';
 import GreetBox from '../greetBox/greetBox';
 import sPic from '../../assets/sPic.jpg';
-import mPraises from '../../assets/mPraises.png';
-import mFollower from '../../assets/mFollower.png';
-import mFollowing from '../../assets/mFollowing.png';
-import mViews from '../../assets/mViews.png';
-import star from '../../assets/star.png';
 import { connect } from 'react-redux';
 import './style.css';
 
 class profilePage extends Component {
 	topBoxGen = () => {
 		return(
-			<div id={'topBox'}>
+			<div id={'topBoxProfilePage'}>
 				<img id={'pImg'} src={sPic} alt={'pic-1'}/>
 		        		<div id={'n'}>
 							{this.props.credentials.data.fname + ' '}
@@ -24,54 +19,21 @@ class profilePage extends Component {
             </div>
 		);
 	};
-
-	midBoxGen = () => {
-		return(
-			<div id={'midBox'}>
-				<div id='detailWrapper'>
-					<img id={'mFollower'} src={mFollower} alt={'pic-5'}/>
-					<div>{this.props.credentials.data.followers}</div>
-					<img id={'mFollowing'} src={mFollowing} alt={'pic-6'}/>
-					<div>{this.props.credentials.data.following}</div>
-					<img id={'mPraises'}  src={mPraises} alt={'pic-7'}/>
-					<div>{this.props.credentials.data.praises}</div>
-					<img id={'mViews'} src={mViews} alt={'pic-8'}/>
-					<div>{this.props.credentials.data.views}</div>
-				</div>
-			</div>
-		);
-	};
-
-	bottomBoxGen = () => {
-		return (
-			<div id={'bottomBox'}>
-				<p id={'bio'}>Bio: {this.props.credentials.data.bio}</p>
-				<div id={'db'}>Date Of Birth: {this.props.credentials.data.dob}</div>
-				<div id={'place'}>Place: {this.props.credentials.data.country}</div>
-				<div id={'prof'}>Profession: {this.props.credentials.data.profession}</div>
-				<div id={'ed-acct'}>
-					<button id={'edit'} className='grow'>Edit Profile</button>
-				</div>
-			</div>
-		);
-	};
-
 	render() {
-       return (
-       	<div>
-       		<TopMostBar calledFrom={'profilePage'}/>
-			<GreetBox/>
-       		<div id={'profileBoxWrapper'}>
-				{this.topBoxGen()}
-				{this.midBoxGen()}
-				{this.bottomBoxGen()}
+       	return (
+       		<div>
+       			<TopMostBar calledFrom={'profilePage'}/>
+				<GreetBox/>
+       			<div id={'profileBoxWrapper'}>
+					{this.topBoxGen()}
+				</div>
+				<PageFooter/>
 			</div>
-			<PageFooter/>
-		</div>
-	   );
+	  	 );
 	};
-}	
+}		
 
+	
 const mapStateToProps = (state) => {
 	return {
 		credentials: state.credentials
