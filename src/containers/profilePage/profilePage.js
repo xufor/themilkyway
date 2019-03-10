cea7a0a4cfba4f768d3ec6502756df85c341af08
Xufor <lucky1612000@gmail.com>
2019-03-10 23:59:04 +0530

    Imp changes.

--- src/containers/profilePage/profilePage.js
+++ src/containers/profilePage/profilePage.js
@@ -6,20 +6,18 @@ import sPic from '../../assets/samplePic.png';
 import { connect } from 'react-redux';
 import './style.css';
 
-//dont use vh in css anymore, use % instead, use of vh should be avoided by all means
-
 class profilePage extends Component {
 	topBoxGen = () => {
 		return(
 			<div id={'topBoxProfilePage'}>
 				<img id={'profileImgOnProfilePage'} src={sPic} alt={'pic-1'}/>
-				<div id={'nameAndImgWrapperOnProfilePage'}>
+				<div id={'nameLineBtnWrapperOnProfilePage'}>
 					<div id={'nameOnProfilePage'}>
 							{this.props.credentials.data.fname + ' '}
 							{this.props.credentials.data.sname}
 					</div>
-					<div className={'ed-acct grow'}>Edit</div>
-					<div id={'lineOnProfilePage'}/>
+					<div id={'editButtonOnProfilePage'} className={'grow'}>Edit</div>
+					<div id={'lineOnProfilePage'} className={'shadow-4'}/>
 				</div>
             </div>
 		);
@@ -27,7 +25,7 @@ class profilePage extends Component {
 
     basicInfo = () => {
     	return(
-    		<div id={'basicInfoProfilepage'}>
+    		<div id={'basicInfoProfilePage'} className={'shadow-4'}>
     			<div id={'dateEmailInfo'}>
     				<div id={'dob'}>
     					{this.props.credentials.data.dob}
@@ -42,8 +40,8 @@ class profilePage extends Component {
 
     achievementsInfo = () => {
     	return(
-    		<div id={'achievementsInfoProfilepage'}>
-    			<div id={'folmillikeviewInfo'}>
+    		<div id={'achievementsInfoProfilePage'} className={'shadow-4'}>
+    			<div id={'statsOnProfilePageWrapper'}>
     				<div id={'follower'}>Followers: {this.props.credentials.data.followers}</div>
     				<div id={'milestones'}>Milestones: {this.props.credentials.data.milestones}</div>
     				<div id={'likes'}>Likes: {this.props.credentials.data.praises}</div>
@@ -55,7 +53,7 @@ class profilePage extends Component {
 
     advancedInfo = () => {
     	return(
-           <div id={'advancedInfoProfilepage'}>
+           <div id={'advancedInfoProfilePage'} className={'shadow-4'}>
     			<div id={'folBioPrivacy'}>
     				<div id={'following'}>Following: {this.props.credentials.data.following}</div>
     				<div id={'bio'}>Bio: {this.props.credentials.data.bio}</div>
