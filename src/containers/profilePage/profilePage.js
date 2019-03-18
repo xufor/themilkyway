96f51fa6c0e0f61e8c45117d936efcbc102b2dff
Xufor <lucky1612000@gmail.com>
2019-03-19 00:30:49 +0530

    More Refactoring.

--- src/containers/profilePage/profilePage.js
+++ src/containers/profilePage/profilePage.js
@@ -8,13 +8,14 @@ import './style.css';
 
 class profilePage extends Component {
 	topBoxGen = () => {
+		let { firstName, lastName} = this.props.credentials.data;
 		return(
 			<div id={'profileBoxUpperRegion'}>
 				<img id={'profileImgOnProfilePage'} className={'shadow-5'} src={sPic} alt={'pic-1'}/>
 				<div id={'nameLineBtnWrapperOnProfilePage'}>
 					<div id={'nameOnProfilePage'}>
-							{this.props.credentials.data.firstName + ' '}
-							{this.props.credentials.data.lastName}
+							{firstName + ' '}
+							{lastName}
 					</div>
 					<div id={'editButtonOnProfilePage'} className={'grow'}>Edit</div>
 					<div id={'lineOnProfilePage'}/>
@@ -24,36 +25,39 @@ class profilePage extends Component {
 	};
 
     basicInfo = () => {
-    	return(
+		let { dob, emailId} = this.props.credentials.data;
+		return(
     		<div id={'basicInfoProfilePage'} className={'shadow-4'}>
     			<div id={'dateEmailInfo'}>
-    				<div id={'dob'}>Birthday: {this.props.credentials.data.dob}</div><br/>
-    				<div id={'email'}>Email: {this.props.credentials.data.emailId}</div>
+    				<div id={'dob'}>Birthday: {dob}</div><br/>
+    				<div id={'email'}>Email: {emailId}</div>
     			</div>
     		</div>
     	);
     };
 
     achievementsInfo = () => {
+    	let { followers, praises, milestones, views} = this.props.credentials.data;
     	return(
     		<div id={'achievementsInfoProfilePage'} className={'shadow-4'}>
     			<div id={'statsOnProfilePageWrapper'}>
-    				<div id={'follower'}>Followers: {this.props.credentials.data.followers}</div><br/>
-    				<div id={'milestones'}>Milestones: {this.props.credentials.data.milestones}</div><br/>
-    				<div id={'likes'}>Likes: {this.props.credentials.data.praises}</div><br/>
-    				<div id={'views'}>Views: {this.props.credentials.data.views}</div>
+    				<div id={'follower'}>Followers: {followers}</div><br/>
+    				<div id={'milestones'}>Milestones: {milestones}</div><br/>
+    				<div id={'likes'}>Likes: {praises}</div><br/>
+    				<div id={'views'}>Views: {views}</div>
     			</div>
     		</div>
     		);
     };
 
     advancedInfo = () => {
+		let { following, bio, privacy} = this.props.credentials.data;
     	return(
            <div id={'advancedInfoProfilePage'} className={'shadow-4'}>
     			<div id={'folBioPrivacy'}>
-    				<div id={'following'}>Following: {this.props.credentials.data.following}</div><br/>
-    				<div id={'bio'}>Bio: {this.props.credentials.data.bio}</div><br/>
-    				<div id={'privacy'}>Privacy Options: {this.props.credentials.data.privacy}</div>
+    				<div id={'following'}>Following: {following}</div><br/>
+    				<div id={'bio'}>Bio: {bio}</div><br/>
+    				<div id={'privacy'}>Privacy Options: {privacy}</div>
     			</div>
     		</div>
     		);
@@ -61,7 +65,7 @@ class profilePage extends Component {
 
 	render() {
        	return (
-       		<div>
+       		<div id={'outerMostWrapperOfProfilePage'}>
        			<TopMostBar calledFrom={'profilePage'}/>
 				<GreetBox/>
        			<div id={'profilePageBoxWrapper'}>
