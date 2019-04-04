6ff060ebd71b5b688f78ab9bc93b48189823a99b
Xufor <lucky1612000@gmail.com>
2019-04-04 18:55:47 +0530

    Added draft.js and removed slate.js.

--- src/containers/composePage/composePage.js
+++ src/containers/composePage/composePage.js
@@ -1,52 +1,22 @@
 import React, { Component } from 'react';
-// import { bindActionCreators } from 'redux';
-// import { connect } from 'react-redux';
-import { Editor } from 'slate-react';
-import { Value } from 'slate';
+import { Editor, EditorState } from 'draft-js';
 import './style.css';
-
-const initialValue = Value.fromJSON({
-    document: {
-        nodes: [
-            {
-                object: 'block',
-                type: 'paragraph',
-                nodes: [
-                    {
-                        object: 'text',
-                        leaves: [
-                            {
-                                text: 'A line of text in a paragraph.',
-                            },
-                        ],
-                    },
-                ],
-            },
-        ],
-    },
-});
 
 class ComposePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: initialValue
+            editorState: EditorState.createEmpty()
         };
     }
 
-    onChange = ({ value }) => {
-        this.setState({ value })
-    };
-
-    onKeyDown = (event) => {
-        console.log(event.key);
+    onChange = (editorState) => {
+        this.setState({editorState});
     };
 
     render() {
         return (
-            <div id={'m-box-compose-pg'}>
-                <Editor value={this.state.value} onChange={this.onChange} onKeyDown={this.onKeyDown}/>
-            </div>
+            <Editor editorState={this.state.editorState} onChange={this.onChange} />
         )
     };
 }
